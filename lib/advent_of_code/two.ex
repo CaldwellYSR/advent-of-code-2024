b307d58bc72854056560d1894f57319514839b47
Matthew Caldwell <caldwellysr@gmail.com>
2024-12-02 13:18:38 -0600

    Refactor code for readability

--- lib/advent_of_code/two.ex
+++ lib/advent_of_code/two.ex
@@ -1,80 +1,52 @@
 defmodule AdventOfCode.Two do
-  def star_one(), do: star_one("two.txt")
+  def star_one(filename \\ "two.txt"), do: process_data(filename, &is_line_safe/1)
+  def star_two(filename \\ "two.txt"), do: process_data(filename, &is_line_safe_with_removals/1)
 
-  def star_one(filename) do
-    AdventOfCode.read_input(filename)
-    |> Stream.map(fn line ->
-      line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-    end)
-    |> Stream.scan(0, fn line, count ->
-      count + is_line_safe(line)
-    end)
-    |> Enum.reverse()
-    |> List.first()
+  def process_data(filename, safety_check) do
+    filename
+    |> AdventOfCode.read_input()
+    |> Stream.map(&parse_line/1)
+    |> Stream.map(safety_check)
+    |> Enum.sum()
   end
 
-  def star_two(), do: star_two("two.txt")
-
-  def star_two(filename) do
-    AdventOfCode.read_input(filename)
-    |> Stream.map(fn line ->
-      line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-    end)
-    |> Stream.scan(0, fn line, count ->
-      count + is_line_safe_with_removals(line)
-    end)
-    |> Enum.reverse()
-    |> List.first()
+  defp parse_line(line) do
+    line
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
   end
 
   def is_line_safe([]), do: 0
 
   def is_line_safe(line) do
-    [first | rest] = line
+    changes = calculate_changes(line)
+    if valid_changes?(changes), do: 1, else: 0
+  end
 
-    line_2 = Enum.reverse([first | Enum.reverse(rest)])
+  def calculate_changes(line) do
+    line
+    |> Enum.zip(tl(line) ++ [List.first(line)])
+    |> List.delete_at(length(line) - 1)
+    |> Enum.map(fn {a, b} -> b - a end)
+  end
 
-    [_ | test_line] =
-      line
-      |> Stream.zip(line_2)
-      |> Enum.to_list()
-      |> Enum.reverse()
-
-    changes =
-      test_line
-      |> Enum.reverse()
-      |> Enum.map(fn {a, b} -> b - a end)
-
-    if Enum.max(changes) <= 3 and
-         Enum.min(changes) >= -3 and
-         (Enum.all?(changes, &(&1 > 0)) or Enum.all?(changes, &(&1 < 0))) do
-      1
-    else
-      0
-    end
+  defp valid_changes?(changes) do
+    Enum.all?(changes, &(&1 in -3..3)) and
+      (Enum.all?(changes, &(&1 > 0)) or Enum.all?(changes, &(&1 < 0)))
   end
 
   def is_line_safe_with_removals([]), do: 0
 
   def is_line_safe_with_removals(line) do
-    case is_line_safe(line) do
-      1 ->
-        1
+    if is_line_safe(line) == 1 or any_safe_removal?(line), do: 1, else: 0
+  end
 
-      0 ->
-        if Enum.map(0..(length(line) - 1), fn item ->
-             {_, rest} = List.pop_at(line, item)
-             rest
-           end)
-           |> Enum.any?(fn line -> is_line_safe(line) == 1 end) do
-          1
-        else
-          0
-        end
-    end
+  defp any_safe_removal?(line) do
+    0..(length(line) - 1)
+    |> Enum.any?(fn i ->
+      line
+      |> List.delete_at(i)
+      |> is_line_safe() == 1
+    end)
   end
 end
